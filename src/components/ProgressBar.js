859fde1ec887a36608e822c2fe9baad9868d28b8
luanr <luan.robert@gec.inatel.br>
2023-05-17 22:44:42 -0300

    Alguns componentes criados

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,9 +1,38 @@
-function ProgressBar() {
-    return (
-      <div>
-        <h1>ProgressBar</h1>
+import React from "react";
+
+const ProgressBar = ({ progress }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "gray",
+        width: "50%",
+        height: "30px",
+        borderRadius: "5px",
+        position: "relative"
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "green",
+          width: `${progress}%`,
+          height: "100%",
+          borderRadius: "5px"
+        }}
+      ></div>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          color: "white",
+          fontSize: "14px"
+        }}
+      >
+        {progress}%
       </div>
-    );
-  }
-  
-  export default ProgressBar;
+    </div>
+  );
+};
+
+export default ProgressBar;
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,17 +1,30 @@
-import Cabecalho from "../partials/cabecalho";
-import Calendario from "../components/calendario"
-import ProgressBar from "../components/ProgressBar"
+import Cabecalho from "../partials/cabecalho";import ProgressBar from "../components/ProgressBar";
+import ProgressItem from "../components/ProgressItem";
+import ProgressItemFilter from "../components/ProgressItemFilter";
 
 function Dashboard() {
+  const progress = 50; // Defina o valor do progresso aqui
     return (
       <div>
         <Cabecalho/>
         <h1>Dados Mensais</h1>
         <div>
-            <Calendario/>
-            <h3>Análise de gastos</h3>
-            <h2>Day</h2>
-            <ProgressBar />
+            <div
+      style={{
+        backgroundColor: "black",
+        width: "60%",
+        height: "170px",
+        borderRadius: "8px",
+        padding: "10px"
+      }}
+    >
+      <h3 style={{ color: "gray" }}>Análise de gastos</h3>
+      <h2 style={{ color: "gray" }}>Day 9 - 15</h2>
+      <ProgressBar progress={progress} />
+    </div>
+
+    <ProgressItem/>
+    <ProgressItemFilter/>
         </div>
       </div>
     );
